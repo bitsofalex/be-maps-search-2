d6d18eeadbf8ac1998018e640718d08c48614355
bitsofalex <1070097+bitsofalex@users.noreply.github.com>
2024-04-23 14:44:05 +1000

    Added typing for result from API and fix failing tests

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,16 +1,51 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from "axios";
+
+interface Place {
+  id: string;
+  type: string;
+  score: number;
+  address: {
+    freeformAddress: string;
+    country: string;
+    countryCode: string;
+    countryCodeISO3: string;
+    countrySubdivision: string;
+    countrySecondarySubdivision: string;
+    municipality: string;
+    municipalitySubdivision: string;
+    streetName: string;
+    streetNumber: string;
+    postalCode: string;
+  };
+  position: {
+    lat: number;
+    lon: number;
+  };
+}
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string) {
-    const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
-        params: {
-            key,
-            limit: 100,
-        }
-    });
-    return autocomplete.data.results.map((result) => {
-        return {
-            placeId: result.id,
-        }
-    })
+export async function getPlaceAutocomplete(
+  key: string,
+  address: string
+): Promise<Place> {
+  const autocomplete: AxiosResponse = await axios.get(
+    `https://api.tomtom.com/search/2/search/${address}.json'`,
+    {
+      params: {
+        key,
+        limit: 100,
+      },
+    }
+  );
+  return autocomplete.data.results.map(({ id, address }: Place) => {
+    return {
+      placeId: id,
+      streetNumber: address.streetNumber,
+      streetName: address.streetName,
+      countryCode: address.countryCode,
+      country: address.country,
+      freeformAddress: address.freeformAddress,
+      municipality: address.municipality,
+    };
+  });
 }

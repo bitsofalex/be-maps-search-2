2f18b83103dfe7107d614b78756b00bfcd5e257c
bitsofalex <1070097+bitsofalex@users.noreply.github.com>
2024-04-23 23:33:58 +1000

    Throw error if api key is not configured

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ export type AddressResult = { placeId: string } & Address;
 export async function getAutoCompleteDetails(
   address: string
 ): Promise<AddressResult[]> {
-  const apiKey = process.env.TOMTOM_API_KEY || "";
+  const apiKey = process.env.TOMTOM_API_KEY;
+
   try {
+    if (!apiKey) {
+      throw new Error("Missing configuration: API key");
+    }
+
     // get autocomplete results
     const places: Place[] = await getPlaceAutocomplete(apiKey, address);
     return places.map(({ id, address }) => {
--- test/index.unit.test.ts
+++ test/index.unit.test.ts
@@ -1,10 +1,18 @@
+import { verify } from "crypto";
 import { getAutoCompleteDetails } from "../src";
 import { getPlaceAutocomplete } from "../src/maps-api";
+import axios from "axios";
 
+jest.mock("axios");
 jest.mock("../src/maps-api");
 
-describe("getPlaceAutocomplete", () => {
+describe("getAutoCompleteDetails", () => {
   const mockedGetPlaceAutocomplete = getPlaceAutocomplete as jest.Mock;
+  const mockAxios = axios as jest.Mocked<typeof axios>;
+  it("returns empty list when api key not configured", async () => {
+    expect(await getAutoCompleteDetails("Charlotte Street")).toStrictEqual([]);
+    expect(mockAxios.get).toHaveBeenCalledTimes(0);
+  });
 
   it("returns empty list when error caught", async () => {
     mockedGetPlaceAutocomplete.mockRejectedValue(
